4817b6d381c62ffad6b5d1e156bfe0f82ebcce10
Konya Donat <konya.donat@gmail.com>
2021-06-15 18:30:47 +0200

    NameNullTest created

--- src/test/java/tests/VideoGameTest.java
+++ src/test/java/tests/VideoGameTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import VideoGame.MMORPGVideoGame;
+import VideoGame.OpenWorldVideoGame;
 import VideoGame.Prototype.WoWMMORPGVideoGame;
 import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
@@ -10,12 +11,21 @@ public class VideoGameTest {
     @Test
     public void NameNullTest(){
 
+        Exception exception = Assertions.assertThrows(RuntimeException.class, ()->{
+            OpenWorldVideoGame openWorldVideoGame = new OpenWorldVideoGame(
+                    null,50,3000,2020,5);
+        });
+        String expectedMessage = "A név nem lehet null!";
+        String actualMessage = exception.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+
     }
 
     @Test
     public void DiscountBy50Test(){
         WoWMMORPGVideoGame woWMMORPGVideoGame = new WoWMMORPGVideoGame(
-                "World of Wacraft",5,13000,2018,3500);
+                "World of Warcraft",5,13000,2018,3500);
         woWMMORPGVideoGame.EditPriceByPercentage(-50);
         Assertions.assertEquals(6500,woWMMORPGVideoGame.getPrice());
 
