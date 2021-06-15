c36f045ce2aff30664b8c41ca72c41d352f0b0fa
Konya Donat <konya.donat@gmail.com>
2021-06-15 18:14:20 +0200

    VideoGameTest created

--- src/test/java/tests/VideoGameTest.java
+++ src/test/java/tests/VideoGameTest.java
@@ -1,4 +1,24 @@
 package tests;
 
+import VideoGame.MMORPGVideoGame;
+import VideoGame.Prototype.WoWMMORPGVideoGame;
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+
 public class VideoGameTest {
+
+    @Test
+    public void NameNullTest(){
+
+    }
+
+    @Test
+    public void DiscountBy50Test(){
+        WoWMMORPGVideoGame woWMMORPGVideoGame = new WoWMMORPGVideoGame(
+                "World of Wacraft",5,13000,2018,3500);
+        woWMMORPGVideoGame.EditPriceByPercentage(-50);
+        Assertions.assertEquals(6500,woWMMORPGVideoGame.getPrice());
+
+    }
+
 }
