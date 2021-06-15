df50eccab2a0a12ad89cb47c5aa7a7db409b8dcf
Konya Donat <konya.donat@gmail.com>
2021-06-15 17:45:21 +0200

    Added new field, updated constructor

--- src/main/java/VideoGame/MMORPGVideoGame.java
+++ src/main/java/VideoGame/MMORPGVideoGame.java
@@ -1,4 +1,21 @@
 package VideoGame;
 
 public abstract class MMORPGVideoGame extends BasicVideoGame{
+    public MMORPGVideoGame(String name, int amount, int price, int releaseYear, int subscriptionPrice) {
+        super(name, amount, price, releaseYear);
+        setSubscriptionPrice(subscriptionPrice);
+    }
+
+    private int subscriptionPrice;
+
+
+    public int getSubscriptionPrice() {
+        return subscriptionPrice;
+    }
+
+    public void setSubscriptionPrice(int subscriptionPrice) {
+        if(subscriptionPrice<0)
+            throw new RuntimeException("Az előfizetés ára nem lehet negatív!");
+        this.subscriptionPrice = subscriptionPrice;
+    }
 }
