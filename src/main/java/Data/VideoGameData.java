facbb11fb7106c5e2ff9afee327f6d7addbef1c8
Konya Donat <konya.donat@gmail.com>
2021-06-15 21:22:44 +0200

    VideoGameData finished

--- src/main/java/Data/VideoGameData.java
+++ src/main/java/Data/VideoGameData.java
@@ -1,4 +1,18 @@
 package Data;
 
+import VideoGame.VideoGame;
+
+import java.util.List;
+
 public interface VideoGameData {
+    VideoGame queryVideoGameByName(String name);
+    List<VideoGame> queryVideoGame();
+
+    void removeVideoGameByVideoGame(VideoGame videoGame);
+    void removeVideoGameByName(String name);
+
+    void addVideoGame(VideoGame videoGame);
+    void buyVideoGame(VideoGame videoGame);
+
+    void updateVideoGamePrice(VideoGame videoGame, int percentage);
 }
