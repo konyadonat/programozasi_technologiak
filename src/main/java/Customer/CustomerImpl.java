45af3b0300db963e1d6d90f88bb6c7a8bcd26681
Konya Donat <konya.donat@gmail.com>
2021-06-15 19:13:31 +0200

    CustomerImpl created

---
+++ src/main/java/Customer/CustomerImpl.java
@@ -0,0 +1,54 @@
+package Customer;
+
+import VideoGame.VideoGame;
+
+
+import java.util.List;
+
+public class CustomerImpl implements Customer{
+
+    private String name;
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+    private int age;
+    @Override
+    public int getAge() {
+        return 0;
+    }
+
+    @Override
+    public void setAge(int age) {
+
+    }
+    private String country;
+    @Override
+    public String getCountry() {
+        return null;
+    }
+
+    @Override
+    public void setCountry(String country) {
+
+    }
+    private String address;
+    @Override
+    public String getAddress() {
+        return null;
+    }
+
+    @Override
+    public void setAddress(String address) {
+
+    }
+
+    
+
+
+}
