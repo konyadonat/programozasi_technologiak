3f3687414a0c8f46616869d3bacda94954dcb664
Konya Donat <konya.donat@gmail.com>
2021-06-15 17:27:15 +0200

    Override methods created

--- src/main/java/VideoGame/BasicVideoGame.java
+++ src/main/java/VideoGame/BasicVideoGame.java
@@ -2,4 +2,50 @@ package VideoGame;
 
 public abstract class BasicVideoGame implements VideoGame{
 
+    String name;
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    int amount;
+    @Override
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public void setAmount(int amount) {
+
+    }
+    int price;
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public void setPrice(int price) {
+
+    }
+    int releaseYear;
+    @Override
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    @Override
+    public void setReleaseYear(int releaseYear) {
+
+    }
+
+    @Override
+    public void discountByPercentage(int percentage) {
+
+    }
 }
