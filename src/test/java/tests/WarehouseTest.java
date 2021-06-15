19b609cfa387b56a793580383a97ba4adfe5f954
Konya Donat <konya.donat@gmail.com>
2021-06-16 00:40:15 +0200

    WarehouseTest passed

--- src/test/java/tests/WarehouseTest.java
+++ src/test/java/tests/WarehouseTest.java
@@ -1,4 +1,41 @@
 package tests;
 
+import Customer.CustomerImpl;
+import VideoGame.OpenWorldVideoGame;
+import Warehouse.BasicWarehouse;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class WarehouseTest {
+
+    @Test
+    public void productToBuyerIdTest() {
+        CustomerImpl customer = new CustomerImpl(
+                "Kónya Donát",
+                20,
+                "Hungary",
+                "Bem út 15");
+
+        OpenWorldVideoGame openWorldVideoGame = new OpenWorldVideoGame(
+                "Skyrim",
+                25,
+                3000,
+                2010,
+                20
+        );
+
+        BasicWarehouse.getInstance().addCustomer(customer);
+
+        CustomerImpl customer1 = new CustomerImpl(
+                "Kónya Donát",
+                20,
+                "Hungary",
+                "Bem út 15");
+
+
+        Assertions.assertEquals(
+                BasicWarehouse.getInstance().getCustomerByName(customer.getName()).getName(),
+                customer1.getName()
+        );
+    }
 }
