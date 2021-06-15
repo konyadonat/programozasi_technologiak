379a9e71405fcedfa7cde3f4de0c8020398195ec
Konya Donat <konya.donat@gmail.com>
2021-06-15 23:00:22 +0200

    OrderImpl finished

--- src/main/java/Order/OrderImpl.java
+++ src/main/java/Order/OrderImpl.java
@@ -1,35 +1,53 @@
 package Order;
 
 import Customer.Customer;
+import Data.Memory.MemoryVideoGameData;
 import VideoGame.VideoGame;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderImpl implements Order{
 
+    public OrderImpl(int id, Customer customer, MemoryVideoGameData videogames){
+        this.setId(id);
+        this.setCustomer(customer);
+        this.videogames = videogames;
+    }
+
+    public OrderImpl(Customer customer, MemoryVideoGameData videogames){
+        this.setCustomer(customer);
+        this.videogames = videogames;
+    }
+
+
     private  int id;
     @Override
     public int getId() {
-        return 0;
+        return id;
     }
 
     @Override
     public void setId(int id) {
-
+        this.id = id;
     }
     private Customer customer;
     @Override
     public Customer getCustomer() {
-        return null;
+        return customer;
     }
 
+    private MemoryVideoGameData videogames;
     @Override
     public List<VideoGame> getVideoGamesByCustomer() {
-        return null;
+        List<VideoGame> temp = new ArrayList<>();
+        for(VideoGame v: videogames.queryVideoGame())
+            temp.add(v);
+        return temp;
     }
 
     @Override
     public void setCustomer(Customer customer) {
-
+        this.customer = customer;
     }
 }
