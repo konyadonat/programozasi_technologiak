a3a37fc44e751d3347725220f581e82f1ddfbff7
Konya Donat <konya.donat@gmail.com>
2021-06-15 17:53:05 +0200

    implemented constructor and copy

--- src/main/java/VideoGame/Prototype/WoWMMORPGVideoGame.java
+++ src/main/java/VideoGame/Prototype/WoWMMORPGVideoGame.java
@@ -3,4 +3,15 @@ package VideoGame.Prototype;
 import VideoGame.MMORPGVideoGame;
 
 public class WoWMMORPGVideoGame extends MMORPGVideoGame {
+    public WoWMMORPGVideoGame(String name, int amount, int price, int releaseYear, int subscriptionPrice) {
+        super(name, amount, price, releaseYear, subscriptionPrice);
+    }
+
+    @Override
+    public MMORPGVideoGame copy() {
+        VideoGame.Prototype.WoWMMORPGVideoGame woWMMORPGVideoGame = new VideoGame.Prototype.WoWMMORPGVideoGame(
+                this.getName(),this.getAmount(),this.getPrice(),this.getReleaseYear(),this.getSubscriptionPrice()
+        );
+        return woWMMORPGVideoGame;
+    }
 }
