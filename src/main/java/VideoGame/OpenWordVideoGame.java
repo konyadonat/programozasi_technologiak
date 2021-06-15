ba1b1e2177274825ef23aa4092c70bc1a5e2152e
Konya Donat <konya.donat@gmail.com>
2021-06-15 18:18:26 +0200

    OpenWorldVideoGame abstract class created

---
+++ src/main/java/VideoGame/OpenWordVideoGame.java
@@ -0,0 +1,22 @@
+package VideoGame;
+
+public abstract class OpenWordVideoGame extends BasicVideoGame{
+
+
+    public OpenWordVideoGame(String name, int amount, int price, int releaseYear,int worldSize) {
+        super(name, amount, price, releaseYear);
+        setWorldSize(worldSize);
+    }
+    private int worldSize;
+
+
+    public int getWorldSize() {
+        return worldSize;
+    }
+
+    public void setWorldSize(int worldSize) {
+        if(worldSize < 1)
+            throw new RuntimeException("A világ mérete túl kicsi!");
+        this.worldSize = worldSize;
+    }
+}
