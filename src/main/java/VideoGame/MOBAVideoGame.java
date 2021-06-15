5fc93f340aec6b2a30fa9ef841efbb4fb80b90b1
Konya Donat <konya.donat@gmail.com>
2021-06-15 17:58:50 +0200

    Abstract class created and implemented

---
+++ src/main/java/VideoGame/MOBAVideoGame.java
@@ -0,0 +1,20 @@
+package VideoGame;
+
+public abstract class MOBAVideoGame extends BasicVideoGame{
+    public MOBAVideoGame(String name, int amount, int price, int releaseYear,int teamSize) {
+        super(name, amount, price, releaseYear);
+        setTeamSize(teamSize);
+    }
+
+    private int teamSize;
+
+    public int getTeamSize() {
+        return teamSize;
+    }
+
+    public void setTeamSize(int teamSize) {
+        if(teamSize<2)
+            throw new RuntimeException("A csapat legalaább 2 ember!");
+        this.teamSize = teamSize;
+    }
+}
