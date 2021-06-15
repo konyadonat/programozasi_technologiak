2f28de01d155060a738461250c8cde170d97037d
Konya Donat <konya.donat@gmail.com>
2021-06-15 23:37:10 +0200

    MemoryOrderData finished

--- src/main/java/Data/Memory/MemoryOrderData.java
+++ src/main/java/Data/Memory/MemoryOrderData.java
@@ -1,4 +1,46 @@
 package Data.Memory;
 
-public class MemoryOrderData {
+import Data.OrderData;
+import Order.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryOrderData implements OrderData {
+    public MemoryOrderData(){
+        orders=new ArrayList<>();
+    }
+    private List<Order> orders;
+
+    @Override
+    public void addOrder(Order order) {
+        order.setId(orders.size() + 1);
+        orders.add(order);
+    }
+
+    @Override
+    public Order queryOrderById(int id) {
+        for(Order o : orders){
+            if(o.getId() == id)
+                return o;
+        }
+        throw new RuntimeException("Nincs ilyen order!");
+    }
+
+    @Override
+    public List<Order> queryOrders() {
+        List<Order> temp = new ArrayList<>();
+        for(Order o : orders)
+            temp.add(o);
+        return temp;
+    }
+
+    @Override
+    public void removeOrderById(int id) {
+        for(Order o : orders){
+            if(o.getId() == id)
+                orders.remove(o);
+        }
+        throw new RuntimeException("Nincs ilyen order!");
+    }
 }
