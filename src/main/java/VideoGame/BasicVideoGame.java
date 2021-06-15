531a069cd471b1d2fb13ca5ffbd62d8cb4465c66
Konya Donat <konya.donat@gmail.com>
2021-06-15 17:36:27 +0200

    Fields,method finished

--- src/main/java/VideoGame/BasicVideoGame.java
+++ src/main/java/VideoGame/BasicVideoGame.java
@@ -1,6 +1,7 @@
 package VideoGame;
 
 public abstract class BasicVideoGame implements VideoGame{
+
     public BasicVideoGame(String name, int amount, int price, int releaseYear) {
         setName(name);
         setAmount(amount);
@@ -17,7 +18,13 @@ public abstract class BasicVideoGame implements VideoGame{
 
     @Override
     public void setName(String name) {
-
+        if(name==null)
+            throw new RuntimeException("A név nem lehet null!");
+        if(name.length()<3)
+            throw new RuntimeException("A név hossza legalalább 3 karakter!");
+        if(name.length()>40)
+            throw new RuntimeException("A név hossza maximum 40 karakter! (Ha hosszabb senki nem olvasse el)");
+        this.name=name;
     }
 
     int amount;
@@ -28,7 +35,9 @@ public abstract class BasicVideoGame implements VideoGame{
 
     @Override
     public void setAmount(int amount) {
-
+        if(amount < 1)
+            throw new RuntimeException("A mennyiség legalább 1!");
+        this.amount = amount;
     }
     int price;
     @Override
@@ -38,7 +47,9 @@ public abstract class BasicVideoGame implements VideoGame{
 
     @Override
     public void setPrice(int price) {
-
+        if(price<0)
+            throw new RuntimeException("A játék ára nem lehet negatív!");
+        this.price=price;
     }
     int releaseYear;
     @Override
@@ -48,11 +59,17 @@ public abstract class BasicVideoGame implements VideoGame{
 
     @Override
     public void setReleaseYear(int releaseYear) {
-
+        if(releaseYear<1967)
+            throw new RuntimeException("A megjelenés éve nem lehet kissebb mint 1967! (Első játék megjelenése)");
+        if(releaseYear>2021)
+            throw new RuntimeException("A játék még meg sem jelent!");
+        this.releaseYear=releaseYear;
     }
 
     @Override
     public void discountByPercentage(int percentage) {
-
+        double discountByPercentage = (100 + percentage) / 100.0;
+        int newPrice = (int)(this.getPrice() * discountByPercentage);
+        this.setPrice(newPrice);
     }
 }
