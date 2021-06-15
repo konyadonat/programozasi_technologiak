74778f4fc3a887c35d54cab68c534a7e01c30e18
Konya Donat <konya.donat@gmail.com>
2021-06-15 23:53:08 +0200

    changed SupplyBySupplier paramters

--- src/main/java/Supplier/Supplier.java
+++ src/main/java/Supplier/Supplier.java
@@ -1,10 +1,11 @@
 package Supplier;
 
 import VideoGame.VideoGame;
+import Warehouse.Warehouse;
 
 public interface Supplier {
 
     String getName();
     int getId();
-    void SupplyBySupplier(VideoGame videoGame, )
+    void SupplyBySupplier(VideoGame videoGame, Warehouse w);
 }
