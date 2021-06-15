851195de72ef5f533cfbe90f5e5f61893886179b
Konya Donat <konya.donat@gmail.com>
2021-06-15 19:40:51 +0200

    Finished factoryimpl

--- src/main/java/Customer/CustomerFactoryImpl.java
+++ src/main/java/Customer/CustomerFactoryImpl.java
@@ -1,6 +1,18 @@
 package Customer;
 
 public class CustomerFactoryImpl implements CustomerFactory{
+
+    private static CustomerFactoryImpl customerFactory=null;
+    private CustomerFactoryImpl(){
+
+    }
+
+    public static CustomerFactoryImpl getInstance(){
+        if(customerFactory == null)
+            customerFactory= new CustomerFactoryImpl();
+        return customerFactory;
+    }
+
     @Override
     public Customer createCustomer(String name, int age, String country, String address) {
         return new CustomerImpl(name,age,country,address);
