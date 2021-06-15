34f1f5c6ea7b751bffe183dcc94a742b884d5d5c
Konya Donat <konya.donat@gmail.com>
2021-06-15 19:35:54 +0200

    Fields impelemnted

--- src/main/java/Customer/CustomerImpl.java
+++ src/main/java/Customer/CustomerImpl.java
@@ -10,45 +10,62 @@ public class CustomerImpl implements Customer{
     private String name;
     @Override
     public String getName() {
-        return null;
+        return name;
     }
 
     @Override
     public void setName(String name) {
-
+        if(name==null)
+            throw new RuntimeException("A név nem lehet null!");
+        if (name.length()<2)
+            throw new RuntimeException("A név legalább 2 karakter!");
+        this.name = name;
     }
     private int age;
     @Override
     public int getAge() {
-        return 0;
+        return age;
     }
 
     @Override
     public void setAge(int age) {
+        if (age<=0)
+            throw new RuntimeException("A vásárló életkora nem lehet negatív vagy 0!");
+        if (age>120)
+            throw new RuntimeException("A vásárló életkora nem lehet több mint 120 év!");
+        this.age = age;
 
     }
     private String country;
     @Override
     public String getCountry() {
-        return null;
+        return country;
     }
 
     @Override
     public void setCountry(String country) {
-
+        if(country==null)
+            throw new RuntimeException("A vásárló országa nem lehet null!");
+        if(country.length()<3)
+            throw new RuntimeException("A vásárló oszága legalább 3 karakter hosszú!");
+        this.country = country;
     }
     private String address;
     @Override
     public String getAddress() {
-        return null;
+        return address;
     }
 
     @Override
     public void setAddress(String address) {
-
+        if(address == null)
+            throw new RuntimeException("A vásárló címe nem lehet null!");
+        if(address.length() <= 3)
+                throw new RuntimeException("A vásárló címe legalább 3 karakter!");
+        this.address=address;
     }
 
-    
+
 
 
 }
