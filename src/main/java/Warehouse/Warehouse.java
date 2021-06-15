d06e21b4ae2aba036d68e1ad5bac7d96f7aadef1
Konya Donat <konya.donat@gmail.com>
2021-06-15 23:42:47 +0200

    warehouse interface created and implemented

---
+++ src/main/java/Warehouse/Warehouse.java
@@ -0,0 +1,25 @@
+package Warehouse;
+
+import Customer.Customer;
+import Order.Order;
+import VideoGame.VideoGame;
+
+import java.util.List;
+
+public interface Warehouse {
+    void addVideoGame(VideoGame videoGame);
+    List<VideoGame> getVideoGames();
+    VideoGame getVideoGamesByName(String name);
+
+    void addCustomer(Customer c);
+    List<Customer> getCustomers();
+    Customer getCustomerById(String id);
+
+    void addOrder(Order o);
+    List<Order> getOrders();
+    Order getOrderById(int id);
+    void orderAddToCustomer(String name);
+
+    void addOrderToCustomerByName(String name);
+    void addProductToCustomerByName(VideoGame videoGame,String name);
+}
